c270b5f65a547f3b5b7068a9a6ed817907aaf4b5
Ankush Aggarwal <ankushagg93@gmail.com>
2015-10-04 13:31:21 -0400

    added first program

--- src/ArraysStrings/UniqueCharacterString.java
+++ src/ArraysStrings/UniqueCharacterString.java
@@ -3,7 +3,26 @@ package ArraysStrings;
 public class UniqueCharacterString {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println(isUniqueChar("ball"));
+        System.out.println(isUniqueChar("racket"));
+    }
+
+    /**
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     */
+    public static boolean isUniqueChar(String input) {
+        if (input == null || input.length() > 256)
+            return false;
+        boolean[] charSet = new boolean[256];
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (charSet[c])
+                return false;
+            else
+                charSet[c] = true;
+        }
+        return true;
     }
 
 }
