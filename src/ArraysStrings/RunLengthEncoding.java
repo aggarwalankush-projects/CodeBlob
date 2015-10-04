8ee9cbf0a08b5c267ccfb94667bbc780fe8371d0
Ankush Aggarwal <ankushagg93@gmail.com>
2015-10-04 17:00:02 -0400

    Run Length Encoding

---
+++ src/ArraysStrings/RunLengthEncoding.java
@@ -0,0 +1,41 @@
+package ArraysStrings;
+
+public class RunLengthEncoding {
+
+    public static void main(String[] args) {
+        System.out.println(compress("aabbbbcffff"));
+        System.out.println(compress("crazy"));
+    }
+
+
+    /**
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     */
+    public static String compress(String input) {
+        if (input == null || input.trim().length() == 0)
+            return input;
+        StringBuilder result = new StringBuilder();
+
+        char previous = input.charAt(0);
+        int count = 1;
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == previous) {
+                count++;
+            } else {
+                result.append(previous).append(count);
+                previous = input.charAt(i);
+                count = 1;
+            }
+        }
+
+        result.append(previous).append(count);
+
+        if (result.length() > input.length())
+            return input;
+        else
+            return result.toString();
+    }
+
+
+}
