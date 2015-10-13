7a0f74b2925bc7fa1ff217d090acc432f3a01cbf
Ankush Aggarwal <ankushagg93@gmail.com>
2015-10-13 11:37:46 -0400

    added arrayToBST

---
+++ src/TreesGraphs/SortedArrayToBST.java
@@ -0,0 +1,28 @@
+package TreesGraphs;
+
+/**
+ * Given a sorted (increasing order) array with unique integer elements,
+ * Generate a binary search tree with minimal height.
+ */
+public class SortedArrayToBST {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println("Tree generated with root: " + arrayToBST(array));
+    }
+
+    public static TreeNode arrayToBST(int[] array) {
+        return arrayToBST(array, 0, array.length - 1);
+    }
+
+    private static TreeNode arrayToBST(int[] array, int low, int high) {
+        if (low > high)
+            return null;
+        int mid = low + (high - low) / 2;
+        TreeNode root = new TreeNode(array[mid]);
+        root.left = arrayToBST(array, low, mid - 1);
+        root.right = arrayToBST(array, mid + 1, high);
+        return root;
+    }
+
+}
+
