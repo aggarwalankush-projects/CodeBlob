028ae1a80aff0ca246948bcf3c9d7d644e8c9d3e
Ankush Aggarwal <ankushagg93@gmail.com>
2015-10-14 00:12:41 -0400

    added BinaryTreeToBST

---
+++ src/TreesGraphs/BinaryTreeToBST.java
@@ -0,0 +1,44 @@
+package TreesGraphs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+//@formatter:off
+/**
+ * Given a Binary Tree, convert it to a Binary Search Tree.
+ * The conversion must be done in such a way that keeps the original structure of Binary Tree
+ *
+ *       10
+ *     /   \
+ *    17    1
+ *   / \     \
+ *  3   9    16
+ *
+ */
+//@formatter:on
+
+public class BinaryTreeToBST {
+    public static void main(String[] args) {
+        TreeNode root = TreeUtils.createBinaryTree(10, 17, 1, 3, 9, null, 16);
+        ArrayList<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        Collections.sort(list);
+        TreeNode newRoot = TreeUtils.createBST(listToArray(list));
+        System.out.println("Root of BST is " + newRoot);
+    }
+
+    public static void inorder(TreeNode root, ArrayList<Integer> list) {
+        if (root == null)
+            return;
+        inorder(root.left, list);
+        list.add(root.data);
+        inorder(root.right, list);
+    }
+
+    public static int[] listToArray(ArrayList<Integer> list) {
+        int[] array = new int[list.size()];
+        int j = 0;
+        for (int i : list) array[j++] = i;
+        return array;
+    }
+}
+
