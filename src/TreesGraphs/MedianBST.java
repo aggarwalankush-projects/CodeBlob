93e654512a9526128d779b9776481085ef47dbd0
Ankush Aggarwal <ankushagg93@gmail.com>
2015-10-14 02:32:18 -0400

    added MedianBST

---
+++ src/TreesGraphs/MedianBST.java
@@ -0,0 +1,50 @@
+package TreesGraphs;
+
+import java.util.Stack;
+//@formatter:off
+ /**
+ * Find median in a given BST
+ *
+ *       10
+ *     /   \
+ *    7     13
+ *   / \     \
+ *  3   9    16
+ *
+ */
+//@formatter:on
+
+ public class MedianBST {
+     public static void main(String[] args) {
+         TreeNode root = TreeUtils.createBST(3, 7, 9, 10, 13, 16);
+         System.out.println("Median of given BST: " + findMedian(root, 6));
+     }
+
+     public static Double findMedian(TreeNode root, int size) {
+         if (root == null)
+             return null;
+         boolean even = size % 2 == 0;
+         int mid = size / 2;
+         int median = 0, count = 0;
+         TreeNode current = root;
+         Stack<TreeNode> stack = new Stack<>();
+         while (current != null || !stack.isEmpty()) {
+             if (current != null) {
+                 stack.push(current);
+                 current = current.left;
+             } else {
+                 current = stack.pop();
+                 if (even && count == mid - 1)
+                     median = current.data;
+                 else if (count == mid) {
+                     median += current.data;
+                     break;
+                 }
+                 count++;
+                 current = current.right;
+             }
+         }
+         return even ? median / 2.0 : median;
+     }
+ }
+
