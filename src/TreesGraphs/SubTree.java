35eb3daa2be76ed984b052fe434aaf7fcf12aa08
Ankush Aggarwal <ankushagg93@gmail.com>
2015-10-13 21:48:29 -0400

    added PathSumK, SubTree

---
+++ src/TreesGraphs/SubTree.java
@@ -0,0 +1,80 @@
+package TreesGraphs;
+
+//@formatter:off
+/**
+ * We have two binary trees: T1 and T2
+ * Find if T2 is a subtree of T1.
+ *
+ * T1 -> 10        T2 -> 17
+ *     /   \            /  \
+ *    17    1          3    9
+ *   / \     \
+ *  3   9    16
+ *
+ */
+//@formatter:on
+
+public class SubTree {
+    public static void main(String[] args) {
+        TreeNode t1 = createT1();
+        TreeNode t2 = createT2();
+        System.out.println("Is T2 subtree of T1: " + isSubTree(t1, t2));
+    }
+
+    /**
+     * Time Complexity: O(n + km)
+     * Space Complexity: O(log(n) + log(m))
+     * n - size of T1
+     * m - size of T2
+     * k - occurrences of T2 root in T1
+     */
+    public static boolean isSubTree(TreeNode t1, TreeNode t2) {
+        if (t2 == null)
+            return true;
+        return contains(t1, t2);
+    }
+
+    private static boolean contains(TreeNode t1, TreeNode t2) {
+        if (t1 == null)
+            return false;
+        if (t1.data == t2.data)
+            return match(t1, t2);
+        return contains(t1.left, t2) || contains(t1.right, t2);
+    }
+
+    private static boolean match(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null)
+            return true;
+        if (t1 == null || t2 == null)
+            return false;
+        if (t1.data != t2.data)
+            return false;
+        return match(t1.left, t2.left) && match(t1.right, t2.right);
+    }
+
+
+    public static TreeNode createT1() {
+        TreeNode root = new TreeNode(10);
+        TreeNode node1 = new TreeNode(17);
+        TreeNode node2 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(9);
+        TreeNode node6 = new TreeNode(16);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node2.right = node6;
+        return root;
+    }
+
+    public static TreeNode createT2() {
+        TreeNode root = new TreeNode(17);
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        root.left = node1;
+        root.right = node2;
+        return root;
+    }
+}
+
